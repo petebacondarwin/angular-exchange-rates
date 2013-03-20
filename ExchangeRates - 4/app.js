f8c224a6ff12ae86b94a186b38c1429ef829d875
Pete Bacon Darwin <pete@bacondarwin.com>
2013-03-20 14:57:20 +0000

    Resolve promises together, remove old demos

--- ExchangeRates - 4/app.js
+++ ExchangeRates - 4/app.js
@@ -1,23 +1,26 @@
 angular.module('app', [])
 
-.controller('AppController', function($scope, $http, numberFilter) {
-  var openExchangeRatesUrl = 'http://openexchangerates.org/api/';
-  var apiKey = '?app_id=3f87c49674ef43568cbd86476e86f88f';
+.constant('apiKey', '?app_id=3f87c49674ef43568cbd86476e86f88f')
+
+.constant('openExchangeRatesUrl','http://openexchangerates.org/api/')
+
+.controller('AppController', [
+  '$scope', 'apiKey', 'openExchangeRatesUrl', '$http', 'numberFilter', '$q',
+  function($scope, apiKey, openExchangeRatesUrl, $http, numberFilter, $q) {
   
-  $http.get(openExchangeRatesUrl + 'currencies.json' + apiKey).then(function(response) {
-    $scope.currencyNames = response.data;
-  });
-  
-  $http.get(openExchangeRatesUrl + 'latest.json' + apiKey).then(function(response) {
-    $scope.currencyValues = response.data.rates;
+  var namesPromise = $http.get(openExchangeRatesUrl + 'currencies.json' + apiKey);
+  var ratesPromise = $http.get(openExchangeRatesUrl + 'latest.json' + apiKey);
+
+  $q.all([namesPromise, ratesPromise]).then(function(responses) {
+    $scope.currencyNames = responses[0].data;
+    $scope.currencyValues = responses[1].data.rates;
   });
 
   $scope.getCurrencyLabel = function(code, value) {
     return $scope.currencyNames[code] + " (" + numberFilter(value, 2) + " / USD)";
   };
 
-
   $scope.calcCurrency = function() {
     return $scope.fromVal / $scope.fromCurrency * $scope.toCurrency;
   };
-});
+}]);
