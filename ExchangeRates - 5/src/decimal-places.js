76b9fe689393755a98cb1a0a1d295ee5d7b9a61f
Pete Bacon Darwin <pete@bacondarwin.com>
2013-03-26 11:18:28 +0000

    use local libraries throughout

--- ExchangeRates - 5/src/decimal-places.js
+++ ExchangeRates - 5/src/decimal-places.js
@@ -3,12 +3,21 @@ angular.module('decimal-places', [])
 // A new directive that will limit the number of decimal places that are displayed in an input
 // usage: <input ng-model="xxx" decimal-places="2">
 .directive('decimalPlaces', function() {
+  
   return {
     require: 'ngModel',
     link: function(scope, element, attrs, ngModel) {
-      ngModel.$formatters.push(function(value) {
+
+      function limitDecimals(value) {
         var exponent = Math.pow(10, attrs.decimalPlaces);
         return Math.round(value * exponent)/ exponent;
+      }
+
+      ngModel.$formatters.push(limitDecimals);
+      ngModel.$parsers.push(limitDecimals);
+
+      element.bind('blur', function() {
+        element.val(limitDecimals(element.val()));
       });
     }
   };
